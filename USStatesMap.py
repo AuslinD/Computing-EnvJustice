ec76be9f536f0e67d34daeab1d5bf519242c297b
Austin Deng <austin12709@gmail.com>
2024-07-11 14:11:00 -0500

    working emissions data

--- USStatesMap.py
+++ USStatesMap.py
@@ -1,15 +1,23 @@
 import plotly.express as px
 import pandas as pd
 
-data = pd.read_excel('/home/austin/Downloads/annual_generation_state.xlsx')
-print(data.columns)
+gen_data = pd.read_excel('/home/austin/Downloads/annual_generation_state.xlsx')
+#print(data.columns)
 
-data = data[(data.YEAR == 2022)]
-data = data[data['TYPE OF PRODUCER'] == 'Total Electric Power Industry']
-data = data[data['ENERGY SOURCE'] == 'Total']
-data = data[data['STATE'] != 'US-Total']
-print(data)
+gen_data = gen_data[(gen_data.YEAR == 2022)]
+gen_data = gen_data[gen_data['TYPE OF PRODUCER'] == 'Total Electric Power Industry']
+gen_data = gen_data[gen_data['ENERGY SOURCE'] == 'Total']
+gen_data = gen_data[gen_data['STATE'] != 'US-Total']
+#print(gen_data)
 
+emi_data = pd.read_excel('/home/austin/Downloads/emission_annual.xlsx')
+emi_data = emi_data.rename(columns={'Year' : 'YEAR', 'State': 'STATE'})
+print(emi_data.columns)
+emi_data = emi_data[emi_data.YEAR == 2022]
+emi_data = emi_data[emi_data['Producer Type'] == 'Total Electric Power Industry']
+emi_data = emi_data[emi_data['Energy Source'] == 'All Sources']
+emi_data = emi_data[emi_data['STATE'] != 'US-TOTAL']
+print(emi_data)
 
-fig = px.choropleth(data, locations=data['STATE'], locationmode="USA-states", color='GENERATION (Megawatthours)', scope="usa")
-fig.show()
+#fig = px.choropleth(gen_data, locations=data['STATE'], locationmode="USA-states", color='GENERATION (Megawatthours)', scope="usa")
+#fig.show()
