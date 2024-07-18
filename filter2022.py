04d9ebbd5613339df51e0a95040e82874699c584
Austin Deng <austin12709@gmail.com>
2024-07-18 12:10:00 -0500

    made filter2022.py plot info

--- filter2022.py
+++ filter2022.py
@@ -10,3 +10,9 @@ gen_data = gen_data[gen_data['TYPE OF PRODUCER'] == 'Total Electric Power Indust
 gen_data['GENERATION(TWh)'] = gen_data['GENERATION(Megawatthours)'] / 1000000.0
 
 gen_data.to_csv('filtered_generation_2022.csv')
+
+gen_data = gen_data[gen_data['ENERGY SOURCE'] == 'Total']
+gen_data = gen_data[gen_data['STATE'] != 'US-Total']
+
+fig = px.choropleth(gen_data, locations=gen_data['STATE'], locationmode = "USA-states", color='GENERATION(TWh)', scope="usa")
+fig.show()
